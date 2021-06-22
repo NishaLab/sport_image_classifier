5e3d728ecaf4f66a1f2d744c01b518c8b5b459a1
Nguyễn Đức Hưng <hungnguyenw420@gmail.com>
2021-06-22 17:08:49 +0700

    update

--- random_image_prediction.py
+++ random_image_prediction.py
@@ -68,7 +68,23 @@ train_labels = os.listdir(train_path)
 
 train_labels.sort()
 
-file = "89.jpg"
+# file = "89.jpg"
+# image = cv2.imread(file)
+# image = cv2.resize(image, fixed_size)
+
+# fv_hu_moments = fd_hu_moments(image)
+# fv_haralick   = fd_haralick(image)
+# fv_histogram  = fd_histogram(image)
+
+# global_feature = np.hstack([fv_histogram, fv_haralick, fv_hu_moments])
+# h5f_data = h5py.File(output_path + 'random.h5', 'w')
+# h5f_data.create_dataset('dataset_1', data=np.array(global_features))
+# prediction = clf.predict(global_feature.reshape(1,-1))[0]
+# cv2.putText(image, train_labels[prediction], (20,30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,255,255), 3)
+# plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+# plt.show()
+# Random Forest
+file = "random2.jpg"
 image = cv2.imread(file)
 image = cv2.resize(image, fixed_size)
 
@@ -77,7 +93,7 @@ fv_haralick   = fd_haralick(image)
 fv_histogram  = fd_histogram(image)
 
 global_feature = np.hstack([fv_histogram, fv_haralick, fv_hu_moments])
-h5f_data = h5py.File(output_path + 'random.h5', 'w')
+h5f_data = h5py.File(output_path + 'random2.h5', 'w')
 h5f_data.create_dataset('dataset_1', data=np.array(global_features))
 prediction = clf.predict(global_feature.reshape(1,-1))[0]
 cv2.putText(image, train_labels[prediction], (20,30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,255,255), 3)
